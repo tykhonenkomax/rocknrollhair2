fb369b710a0d631b44a3cbca736ec17308814e27
Max <tykhonenko.work@gmail.com>
2022-12-15 01:20:43 +0200

    1

---
+++ src/components/Go/Go.tsx
@@ -0,0 +1,11 @@
+import styles from './Go.module.scss';
+
+export interface Go {
+}
+
+const Go = ({}: Go) => (
+    <div className={styles.templatesName}>
+        Box Component
+    </div>
+);
+
---
+++ templates/default/TemplateName.tsx
@@ -0,0 +1,11 @@
+import styles from './TemplateName.module.scss';
+
+export interface TemplateName {
+}
+
+const TemplateName = ({}: TemplateName) => (
+    <div className={styles.templatesName}>
+        Box Component
+    </div>
+);
+
